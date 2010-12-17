b84861fe043ac824e11b627a6d1a26c96f71ae83
Drake Justice <djustice.kde@gmail.com>
2010-12-16 23:13:54 -0500

    empty config page ui; try to simplify pkgver string more;

--- src/pages/configpage.cpp
+++ src/pages/configpage.cpp
@@ -162,7 +162,7 @@ void ConfigPage::currentPkgItemChanged(int i)
     QProcess p;
     p.start("pacman -Sp --print-format %v " + ui.pkgList->item(i)->data(60).toString());
     p.waitForFinished();
-    QString pkgVer = QString(p.readAll()).split("\n").last();
+    QString pkgVer = QString(p.readAll()).simplified().split(" ").last();
     ui.pkgVerLabel->setText(pkgVer);
 
     ui.pkgDescLabel->setText(ui.pkgList->item(i)->data(61).toString());

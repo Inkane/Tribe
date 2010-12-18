4d2a8ca9c8d3cee45e6eb4153dc9e24d569c85ce
Drake Justice <djustice.kde@gmail.com>
2010-12-18 08:24:23 -0500

    move marble to 4.6 api, patch created for the 4.5 pkgbuild;

--- src/pages/localepage.cpp
+++ src/pages/localepage.cpp
@@ -66,7 +66,7 @@ void LocalePage::createWidget()
     marble->setShowCompass(false);
     marble->setShowCrosshairs(false);
     marble->setShowGrid(false);
-    marble->addPlacemarkFile(QString(DATA_INSTALL_DIR) + "/marble/data/placemarks/cities.kml");
+    marble->addGeoDataFile(QString(DATA_INSTALL_DIR) + "/marble/data/placemarks/cities.kml");
 
     QFile fp(QString(CONFIG_INSTALL_PATH) + "/timezones");
 

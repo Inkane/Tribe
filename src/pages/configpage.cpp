75030bc152e198fa1a3d0eec023ad5b4a26d8b0a
philm <philm@chakra-project.org>
2010-12-19 23:26:12 +0100

    remove tmp fixes

--- src/pages/configpage.cpp
+++ src/pages/configpage.cpp
@@ -339,8 +339,6 @@ void ConfigPage::setInstallPkgzPage()
     if (ui.stackedWidget->currentIndex() != 2) {
         ui.stackedWidget->setCurrentIndex(2);
         ui.currentPageLabel->setText(i18n("Install Packaged Software"));
-        ui.pkgInstallButton->setEnabled(true);
-        enableNextButton(true);
     } else {
         ui.stackedWidget->setCurrentIndex(0);
         ui.currentPageLabel->setText("");
@@ -380,8 +378,6 @@ void ConfigPage::setDownloadBundlesPage()
     if (ui.stackedWidget->currentIndex() != 3) {
         ui.stackedWidget->setCurrentIndex(3);
         ui.currentPageLabel->setText(i18n("Download Popular Bundles"));
-        ui.bundlesDownloadButton->setEnabled(true);
-        enableNextButton(true);
     } else {
         ui.stackedWidget->setCurrentIndex(0);
         ui.currentPageLabel->setText("");
@@ -398,7 +394,6 @@ void ConfigPage::setBootloaderPage()
     if (ui.stackedWidget->currentIndex() != 6) {
         ui.stackedWidget->setCurrentIndex(6);
         ui.currentPageLabel->setText(i18n("Bootloader Settings"));
-        enableNextButton(true);
     } else {
         ui.stackedWidget->setCurrentIndex(0);
         ui.currentPageLabel->setText("");
@@ -410,7 +405,6 @@ void ConfigPage::setInitRamdiskPage()
     if (ui.stackedWidget->currentIndex() != 1) {
         ui.stackedWidget->setCurrentIndex(1);
         ui.currentPageLabel->setText(i18n("Customize Initial Ramdisk"));
-        enableNextButton(true);
     } else {
         ui.stackedWidget->setCurrentIndex(0);
         ui.currentPageLabel->setText("");

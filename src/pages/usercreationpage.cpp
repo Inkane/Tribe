f63011c16c6bd0d7e967ffa8c5984e4112015e99
philm <philm@chakra-project.org>
2011-01-05 16:13:54 +0100

    remove usb/nfs for real

--- src/pages/usercreationpage.cpp
+++ src/pages/usercreationpage.cpp
@@ -171,9 +171,6 @@ void UserCreationPage::aboutToGoToNext()
 
     m_handler->setHostname(ui.hostname->text());
 
-    QProcess::execute("touch /tmp/tribe_initcpio_enable_usb");
-    QProcess::execute("touch /tmp/tribe_initcpio_enable_nfs");
-
     emit goToNextStep();
 }
 
